b4ab1154fc5900b8561f84e50da0326479ad5885
micavilla <micaella.villa1@gmail.com>
2023-07-28 20:07:35 -0500

    added comments and diversified tests

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// import  inquirer, shape classes, and fs
 const inquirer = require('inquirer');
 const { Circle, Triangle, Square } = require('./lib/shapes')
 const fs = require('fs');
 
+// triggers inquirer prompt questions
 inquirer
   .prompt([
     {
       type: 'input',
       name: 'text',
       message: "Enter up to three characters for your logo's text.",
+      // prevents more than 3 characters for text input
       validate: function(input) {
         if (input.length > 3) {
           console.log("\nMust enter a value of no more than 3 characters");
@@ -33,6 +36,7 @@ inquirer
       choices: ['circle', 'triangle', 'square'],
     },
   ])
+  // creates new objects from shape classes with user input for parameters
   .then((data) => {
     let shape;
     if (data.shape === 'circle') {
@@ -42,7 +46,7 @@ inquirer
     } if (data.shape === 'square') {
       shape = new Square(data.shapeColor, data.textColor, data.text.toUpperCase())
     }
-
+    // calls render function from shape classes to create and write in svg file
     fs.writeFile("./examples/logo.svg", shape.render(), (err, result)=>{
       if (err) throw err;
       console.log('Generated logo.svg');
